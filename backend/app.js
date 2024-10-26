ce4b2371a96a42ca943041264ef84d605048d6f8
himansuhs <himansusekhar2002@gmail.com>
2024-10-26 15:08:31 +0530

    sa

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://final-organ-donation.vercel.app/"],
     credentials: true,
   })
 );
